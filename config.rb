7c7ecd59258934c8e493cdac1e6238635ea893a2
Daniel Senff <mail@danielsenff.de>
2020-08-05 21:53:08 +0200

    extend build pipeline

--- config.rb
+++ config.rb
@@ -9,12 +9,41 @@ set :images_dir, 'assets/images'
 set :js_dir,     'assets/javascripts'
 
 #activate :minify_css
+configure :build do
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source/assets/images/logos/')
+    f.output_dir    = File.join(root, 'build')
+    f.icons = {
+      'favicon_base.png' => [
+        { icon: 'chrome-touch-icon-192x192.png' },
+        { icon: 'apple-touch-icon.png', size: '152x152' },
+        { icon: 'ms-touch-icon-144x144-precomposed.png', size: '144x144' },
+        { icon: 'favicon-196x196.png' },
+        { icon: 'favicon-160x160.png' },
+        { icon: 'favicon-96x96.png' },
+        { icon: 'favicon-32x32.png' },
+        { icon: 'favicon-16x16.png' },
+        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' }
+      ]
+    }
+  end
+
+  activate :asset_hash
+  activate :gzip
+  #activate :minify_css
+  activate :minify_html, remove_input_attributes: false
+
+  set :relative_links, true
+  activate :relative_assets
+end
+
 
 activate :deploy do |deploy|
   deploy.deploy_method = :rsync
   deploy.host = 'www.unmedial.de'
   deploy.path = 'racecar-2020/'
   deploy.user = 'u19168'
+  deploy.build_before  = true # always use --no-clean options
   # Optional Settings
   # deploy.port  = 5309 # ssh port, default: 22
   # deploy.clean = true # remove orphaned files on remote host, default: false

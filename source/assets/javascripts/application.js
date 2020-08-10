8487fdb22da989c5f986645d39efef7f025d7bb6
Daniel Senff <mail@danielsenff.de>
2020-08-10 17:34:14 +0200

    add lightbox gallery for aston martin

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
 import './vendor/bootstrap.min.js';
 import popper from 'popper.js';
-import lightbox2 from 'lightbox2';
+import 'lightbox2';
 import * as theme from './theme.js';

5929e7b3fee7e6c7a0fb9cdcf4f49e7c87a5283e
Daniel Senff <mail@danielsenff.de>
2022-02-03 08:19:25 +0100

    have lightbox work again with webpack

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -2,6 +2,7 @@ import 'bootstrap';
 import * as theme from './theme.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../webfonts/fonts.css';
+import 'lightbox2';
+import 'lightbox2/dist/css/lightbox'
 import '../css/theme.css';
 import '../css/timeline.css';
-
